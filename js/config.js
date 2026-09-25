2d3bd31de69da268667f340b768d534d5ef63e9d
FAMIMUEBLES16 <famimuebles16@gmail.com>
2026-09-24 19:42:22 -0500

    Actualizar API publica del tunel: https://stations-strand-example-lions.trycloudflare.com

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const LOCAL_API_URL = 'http://127.0.0.1:8024/api';
-const PUBLIC_API_URL = 'https://buddy-virtually-northeast-customized.trycloudflare.com/api';
+const PUBLIC_API_URL = 'https://stations-strand-example-lions.trycloudflare.com/api';
 
 const configuredApiBase = String(
   globalThis.FAMIMUEBLES_API_BASE_URL ||
